ca102ded4e76ea1c4c391f94670e2ec060ae9a76
CaneCpmNuker <rjaymodestobruno28@gmail.com>
2024-08-07 10:55:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ from rich.text import Text
 from rich.style import Style
 from cpmnuker import CPMNuker
 
-__CHANNEL_USERNAME__ = "CPMNuker"
-__GROUP_USERNAME__   = "CPMNukerChat"
+__CHANNEL_USERNAME__ = "CPMCANETOOL"
+__GROUP_USERNAME__   = "EWANXCANE COMMUNITY"
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
@@ -53,7 +53,7 @@ def banner(console):
     ]
     colorful_text = gradient_text(brand_name, colors)
     console.print(colorful_text)
-    console.print("[bold green]♕ CPMNuker[/bold green]: Car Parking Multiplayer Hacking Tool.")
+    console.print("[bold green]♕ CPMCANETOOL[/bold green]: Car Parking Multiplayer Hacking Tool.")
     console.print(f"[bold green]♕ Telegram[/bold green]: [bold blue]@{__CHANNEL_USERNAME__}[/bold blue] or [bold blue]@{__GROUP_USERNAME__}[/bold blue].")
     console.print("[bold red]==================================================[/bold red]")
     console.print("[bold yellow]! Note[/bold yellow]: Logout from CPM before using this tool !.", end="\n\n")
